f90de6f50378f4eda099074fcc2806f135ade5a4
59naga <i59naga@icloud.com>
2020-12-22 11:24:51 +0900

    :tulip: initial

--- src/index.js
+++ src/index.js
@@ -1,10 +1,35 @@
 import dictItems from "./dict/items.json";
 import dictAreas from "./dict/areas.json";
+import dictAnimals from "./dict/animals.json";
 import localeJa from "./locales/ja.json";
 
 const imagepPrefixUrl =
   "https://static.wikia.nocookie.net/blacksurvivaleternalreturn_gamepedia_en/images/";
 const imageSuffixUrl = "/revision/latest/scale-to-width-down/100";
+const defaultItemNames = [
+  "water",
+  "bread"
+  // default weapons
+  // "kitchen knife",
+  // "rusty sword",
+  // "hatchet",
+  // "walther ppk",
+  // "fedorova",
+  // "long rifle",
+  // "needle",
+  // "short spear",
+  // "hammer",
+  // "short rod",
+  // "baseball",
+  // "razor",
+  // "bow",
+  // "short crossbow",
+  // "cotton glove",
+  // "bamboo",
+  // "starter guitar",
+  // "steel chain",
+];
+
 export function find(...args) {
   return findById(...args);
 }
@@ -15,9 +40,129 @@ export function findById(id) {
   }
   return null;
 }
-
 export function findOne(conditions = {}, options = {}) {
   return (findAll(conditions, { ...options, limit: 1 }) || [])[0];
+}
+export function findParents(children, conditions = {}, options = {}) {
+  const parents = findAll({ children }, { ...options });
+
+  // 検索対象はツリーから除外する
+  const childIds = (typeof children === "string" ? [children] : children).map(id => {
+    return id.toLowerCase();
+  });
+  parents.forEach(parent => {
+    parent.children.forEach((child, index) => {
+      if (childIds.indexOf(child.id) > -1) {
+        parent.children.splice(index, 1);
+      }
+    });
+  });
+
+  return parents;
+}
+export function findChildren(parents, conditions = {}, options = {}) {
+  const children = findAll({ parents }, { ...options });
+
+  // 検索対象はツリーから除外する
+  const parentIds = (typeof parents === "string" ? [parents] : parents).map(id => {
+    return id.toLowerCase();
+  });
+  children.forEach(child => {
+    child.parents.forEach((parent, index) => {
+      if (parentIds.indexOf(parent.id) > -1) {
+        child.parents.splice(index, 1);
+      }
+    });
+  });
+
+  return children;
+}
+export function findCreatableAreas(id, withOutMaterials = []) {
+  const creatables = [];
+
+  let materials = findById(id).getMaterials();
+  if (!materials.length) {
+    return creatables;
+  }
+  materials = materials.filter(material => {
+    return defaultItemNames.concat(toArray(withOutMaterials)).indexOf(material.id) === -1;
+  });
+
+  const [{ areas: firstAreas }, ...anotherItems] = materials;
+  const allMatchedAreaNames = firstAreas
+    .filter(firstArea => {
+      const anotherItemMatched = anotherItems.filter(({ id, areas: anotherAreas }) => {
+        const found = anotherAreas.find(anotherArea => anotherArea.name === firstArea.name);
+        if (!found) {
+          return false;
+        }
+        return found.amount != null || found.animals.length;
+      });
+
+      return anotherItems.length === anotherItemMatched.length;
+    })
+    .map(area => area.name);
+
+  allMatchedAreaNames.forEach(areaName => {
+    creatables.push(
+      areaName,
+      materials.map(item => {
+        const area = item.areas.find(area => area.name === areaName);
+        return [
+          item.id,
+          area.amount,
+          ...area.animals.map(animal => [animal.name, animal.rate].join(","))
+        ];
+      })
+    );
+  });
+
+  return creatables;
+}
+export function findCreatableHighRarityItems() {
+  const highRarityItems = findAll({ rarity: { gte: 2 }, children: {gte: 1} });
+
+  const areas = {}
+  dictAreas.forEach(area => {
+    const [name, materials] = area;
+    const materialNames = [];
+    materials
+      .map(([materialName, materialCount]) => {
+        const [, animalMaterials] = dictAnimals.find(
+          ([animalName]) => materialName === animalName
+        ) || [null, []];
+        const realisticMaterials = animalMaterials.filter(([materialName, rate]) => rate >= 0.3);
+        return [materialName, materialCount, realisticMaterials];
+      })
+      .filter(([materialName, materialCount, realisticMaterials]) => {
+        return materialCount > 0 || realisticMaterials.length;
+      })
+      .forEach(([materialName, materialCount, realisticMaterials]) => {
+        if (realisticMaterials.length) {
+          realisticMaterials.map(([materialName]) => {
+            if (materialNames.indexOf(materialName) > -1) {
+              return;
+            }
+            materialNames.push(materialName);
+          });
+        } else {
+          materialNames.push(materialName);
+        }
+      });
+
+    const creatables = highRarityItems.filter(item => {
+      if(!item.getMaterials) {
+        return false
+      }
+      const names = item.getMaterials().map(material => material.id);
+      const nameMatched = names.filter(name => materialNames.indexOf(name) > -1)
+      return names.length == nameMatched.length
+    })
+
+    areas[name] = creatables
+  });
+
+  return areas
 }
 export function findAll(conditions = {}, options = {}) {
   let found = dictItems
@@ -25,39 +170,74 @@ export function findAll(conditions = {}, options = {}) {
       const props = getItemProperties(itemArray);
       return getCondition(props, conditions);
     })
+    .filter((itemArray, index) => {
+      if (options.limit > 0 && options.offset > 0) {
+        return index >= options.offset && index < options.offset + options.limit;
+      } else if (options.limit > 0) {
+        return index < options.limit;
+      } else if (options.offset > 0) {
+        return index >= options.offset;
+      }
+      return true;
+    })
     .map(itemArray => new Item(itemArray));
-
-  if (options.limit > 0 && options.offset > 0) {
-    found = found.slice(~~options.offset, ~~options.offset + ~~options.limit);
-  } else if (options.limit > 0) {
-    found = found.slice(0, ~~options.limit);
-  } else if (options.offset > 0) {
-    found = found.slice(~~options.offset);
-  }
 
   return found;
 }
-function getCondition(props, conditions) {
-  if (conditions.type != null && conditions.type !== props.type) {
+function getCondition(props, conditions = {}) {
+  let { stats, type, typeSub, rarity, parents, children } = conditions;
+  if (typeof stats === "string") {
+    stats = new RegExp(stats);
+  }
+  if (typeof parents === "string") {
+    parents = [parents];
+  }
+  if (typeof children === "string") {
+    children = [children];
+  }
+
+  // 不合致したものにfalseを返す
+  if (type != null && type !== props.type) {
     return false;
   }
-  if (conditions.typeSub != null && conditions.typeSub !== props.typeSub) {
+  if (typeSub != null && typeSub !== props.typeSub) {
     return false;
   }
-  if (conditions.rarity != null && conditions.rarity !== props.rarity) {
+  if (typeof rarity === "number" && rarity !== props.rarity) {
     return false;
   }
-  if (typeof conditions.stats === "string" && conditions.stats !== props.stats) {
+  if (rarity != null && rarity.gte != null && rarity.gte > props.rarity) {
     return false;
   }
-  if (conditions.stats instanceof RegExp && props.stats.match(conditions.stats) == null) {
+  if (stats instanceof RegExp && props.stats.match(stats) == null) {
     return false;
   }
+  if (
+    parents instanceof Array &&
+    !parents.find(parent => {
+      return props.parents.indexOf(String(parent).toLowerCase()) > -1;
+    })
+  ) {
+    return false;
+  }
+  if (
+    children instanceof Array &&
+    !children.find(child => {
+      return props.children.indexOf(String(child).toLowerCase()) > -1;
+    })
+  ) {
+    return false;
+  }
+  if (children != null && children.gte != null && children.gte > props.children.length) {
+    return false;
+  }
+
+  // 条件による絞込みが存在した場合は合致したものだけtrueとなる
   return true;
 }
 
 class Item {
-  constructor(id, includeParents = true, includeChildren = true) {
+  constructor(id, includeParents = true, includeChildren = true, includeArea = true) {
     let itemArray = id;
     if (typeof id === "string") {
       const key = String(id).toLowerCase();
@@ -76,9 +256,53 @@ class Item {
       props.children = props.children.map(child => new Item(child, false, true));
     }
 
+    if (includeArea) {
+      dictAreas.forEach(area => {
+        const [name, materials] = area;
+
+        let amount = null;
+        materials.forEach(([materialName, materialAmount]) => {
+          if (materialName !== props.id) {
+            return;
+          }
+          if (!materialAmount) {
+            return;
+          }
+          amount = materialAmount;
+        });
+
+        const animals = getAnimals(props.id, name);
+        if (amount != null || animals.length) {
+          props.areas.push({ name, amount, animals });
+        }
+      });
+    }
+
     Object.keys(props).forEach(key => {
       this[key] = props[key];
     });
+  }
+  getSrc() {
+    return `${imagepPrefixUrl}${this.src}`;
+  }
+  getSrcThumbnail() {
+    return `${imagepPrefixUrl}${this.src}${imageSuffixUrl}`;
+  }
+  getMaterials() {
+    const materials = [];
+
+    this.children.forEach(child => {
+      if (child.children && child.children.length) {
+        child.getMaterials().forEach(child => {
+          materials.push(child);
+        });
+        return;
+      }
+
+      materials.push(child);
+    });
+
+    return materials;
   }
 }
 
@@ -97,6 +321,7 @@ export function getItemProperties(itemArray) {
     quantity,
     parents,
     children,
+    areas: [],
     ja: {
       id: getLocaleJa(id),
       type: getLocaleJa(type),
@@ -104,6 +329,41 @@ export function getItemProperties(itemArray) {
       stats: getLocaleJa(stats)
     }
   };
+}
+export function getAnimals(propId, areaName) {
+  const animals = [];
+
+  dictAnimals.forEach(animal => {
+    const [animalName, drops, spawnTimer] = animal;
+    drops.forEach(drop => {
+      const [dropName, rate] = drop;
+      if (dropName !== propId) {
+        return;
+      }
+
+      dictAreas.forEach(area => {
+        const [name, materials] = area;
+        if (name !== areaName) {
+          return;
+        }
+        materials.forEach(([materialName, count]) => {
+          if (materialName !== animalName) {
+            return;
+          }
+          if (!count) {
+            return;
+          }
+          if (rate < 0.3) {
+            // ドロップ率が稀のものをエリアの素材として扱わない
+            return;
+          }
+          animals.push({ name: animalName, count, rate });
+        });
+      });
+    });
+  });
+
+  return animals;
 }
 export function getLocaleJa(str) {
   if (str == null || !str) {
@@ -115,4 +375,7 @@ export function getLocaleJa(str) {
     // console;.log(`key "${key}" undefined in src/locales/ja.json`)
   }
   return locale || str;
+}
+export function toArray(strOrArray) {
+  return strOrArray instanceof Array ? strOrArray : [strOrArray];
 }
--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@ import { deepStrictEqual } from "assert";
 
 import * as finder from "../src";
 
-it("ジャガイモの情報と子孫のツリー情報を返すべき", async () => {
+it("ジャガイモの情報と子孫のツリー情報を返すべき", () => {
   const item = finder.find("Potato");
   deepStrictEqual(
     item.parents.map(parent => parent.id),
@@ -10,21 +10,242 @@ it("ジャガイモの情報と子孫のツリー情報を返すべき", async (
   );
 });
 
-it("包帯", async () => {
-  const item = finder.find("Bandage");
+it("致命を検索", () => {
+  const found = finder.findAll({ stats: /healing reduction/ });
   deepStrictEqual(
-    item.parents.map(parent => parent.id),
-    ["patched robe", "squad leader armband", "bracer", "first aid kit", "oilcloth"]
+    found.map(item => item.id),
+    [
+      "rocker's jacket",
+      "sword stopper",
+      "white rhinos",
+      "gilded quill fan",
+      "white crane fan",
+      "harpe",
+      "divine dual swords",
+      "fangtian huaji",
+      "goblin bat",
+      "mallet",
+      "spiky bouncy ball",
+      "ruthenium marble",
+      "composite bow",
+      "twinbow",
+      "elemental bow",
+      "sharper",
+      "the smiting dragon"
+    ]
   );
 });
 
-it("灰", async () => {
-  const ash = finder.find("Ash");
+it("皮の入手場所を返すべき(30%以上)", () => {
+  const item = finder.find("leather");
+  deepStrictEqual(
+    item.areas.map(area => area.name),
+    [
+      "beach",
+      "uptown",
+      "dock",
+      "hotel",
+      "archery range",
+      "school",
+      "forest",
+      "chapel",
+      "cemetery",
+      "factory",
+      "alley",
+      "avenue",
+      "pond",
+      "hospital",
+      "temple"
+    ]
+  );
 });
 
-it("致命を検索", async () => {
-  const found = finder.findAll({ stats: /healing reduction/ });
-  console.log(found);
-  // const common = finder.findAll({ rarity: 1, type: "material" });
-  // console.log(common);
+it("パウンドケーキが作成できるエリアを返すべき", () => {
+  const found = finder.findCreatableAreas("pound cake");
+  deepStrictEqual(found, [
+    "chapel",
+    [
+      ["milk", 3],
+      ["branch", 5]
+    ],
+    "avenue",
+    [
+      ["milk", 6],
+      ["branch", 4]
+    ],
+    "hospital",
+    [
+      ["milk", 5],
+      ["branch", 4]
+    ]
+  ]);
+});
+it("ガントレットが作成できるエリアを返すべき(手袋無視)", () => {
+  const found = finder.findCreatableAreas("gauntlet", "cotton glove");
+  deepStrictEqual(found, [
+    "hotel",
+    [
+      ["scrap metal", 8],
+      ["iron ore", 7]
+    ]
+  ]);
+});
+
+it("エリア単体で作成可能な上位アイテムを返すべき", () => {
+  const found = finder.findCreatableHighRarityItems();
+  deepStrictEqual(
+    found.beach.map(item => item.id),
+    [
+      "canned cod liver",
+      "safety helmet",
+      "leather shield",
+      "chain scythe",
+      "beretta m92f",
+      "wooden bow",
+      "gold",
+      "ruby",
+      "telephoto camera"
+    ]
+  );
+});
+
+it("パン派生で作れるもの一覧を返すべき", () => {
+  const found = finder.findParents("Bread");
+  const recipes = found.map(item => {
+    const list = [item.id];
+    item.children.forEach(child => {
+      list.push([
+        child.id,
+        ...child.areas.map(({ name, amount, animals }) => {
+          return [
+            name,
+            amount ||
+              animals.map(({ name, count, rate }) => {
+                return [name, count, rate];
+              })
+          ];
+        })
+      ]);
+    });
+    return list;
+  });
+  deepStrictEqual(recipes, [
+    ["garlic bread", ["garlic", ["cemetery", 5], ["alley", 6], ["temple", 6]]],
+    ["bungeoppang", ["carp", ["forest", 2], ["cemetery", 2], ["pond", 9]]],
+    ["choco pie", ["chocolate", ["uptown", 5], ["archery range", 4], ["avenue", 6]]],
+    ["bun", ["coffee", ["uptown", 5], ["dock", 6], ["cemetery", 5]]],
+    [
+      "hamburger",
+      [
+        "meat",
+        [
+          "beach",
+          [
+            ["bear", 3, 1],
+            ["boar", 3, 1]
+          ]
+        ],
+        [
+          "uptown",
+          [
+            ["bear", 2, 1],
+            ["chicken", 5, 0.5]
+          ]
+        ],
+        [
+          "dock",
+          [
+            ["bear", 3, 1],
+            ["chicken", 4, 0.5]
+          ]
+        ],
+        [
+          "hotel",
+          [
+            ["chicken", 3, 0.5],
+            ["wolf", 4, 1]
+          ]
+        ],
+        [
+          "archery range",
+          [
+            ["boar", 3, 1],
+            ["wolf", 4, 1]
+          ]
+        ],
+        ["school", [["chicken", 6, 0.5]]],
+        [
+          "forest",
+          [
+            ["boar", 4, 1],
+            ["wolf", 2, 1]
+          ]
+        ],
+        [
+          "chapel",
+          [
+            ["boar", 3, 1],
+            ["chicken", 6, 0.5]
+          ]
+        ],
+        [
+          "cemetery",
+          [
+            ["boar", 4, 1],
+            ["wolf", 2, 1]
+          ]
+        ],
+        [
+          "factory",
+          [
+            ["chicken", 3, 0.5],
+            ["wolf", 4, 1]
+          ]
+        ],
+        [
+          "alley",
+          [
+            ["bear", 3, 1],
+            ["chicken", 8, 0.5],
+            ["wolf", 4, 1]
+          ]
+        ],
+        [
+          "avenue",
+          [
+            ["boar", 6, 1],
+            ["chicken", 5, 0.5]
+          ]
+        ],
+        [
+          "pond",
+          [
+            ["bear", 2, 1],
+            ["boar", 4, 1]
+          ]
+        ],
+        [
+          "hospital",
+          [
+            ["chicken", 3, 0.5],
+            ["wolf", 4, 1]
+          ]
+        ],
+        [
+          "temple",
+          [
+            ["bear", 2, 1],
+            ["boar", 3, 1]
+          ]
+        ]
+      ]
+    ],
+    ["potato bread", ["potato", ["chapel", 4], ["alley", 4], ["temple", 8]]],
+    ["citrus cake", ["lemon", ["uptown", 5], ["hotel", 6], ["hospital", 4]]],
+    ["egg bun", ["egg", ["archery range", 5], ["forest", 6], ["cemetery", 5]]],
+    ["curry bun", ["curry powder", ["uptown", 5], ["factory", 5]]],
+    ["mocha bread", ["coffee liqueur"]],
+    ["pound cake", ["butter"]],
+    ["carp bread", ["carp", ["forest", 2], ["cemetery", 2], ["pond", 9]]]
+  ]);
 });
